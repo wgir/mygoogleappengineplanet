c0ad78607494570fa8151adca9573fc230436d44
wgiraldo <williamgustavo@gmail.com>
2019-09-06 15:30:33 -0400

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mygoogleappengineplanet
+
+This repository is used in the following blog post: https://mydeveloperplanet.com/2019/04/10/deploy-spring-boot-app-to-gcp-app-engine/
---
+++ src/main/java/com/mydeveloperplanet/mygoogleappengineplanet/mygoogleappengineplanet/MyGoogleAppEnginePlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.mygoogleappengineplanet.mygoogleappengineplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoogleAppEnginePlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGoogleAppEnginePlanetApplication.class, args);
+	}
+
+}
